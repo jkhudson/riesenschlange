6515632cc9facec01325e15a864519b397fafc22
Joseph Hudson <joseph.k.hudson@gmail.com>
2015-01-20 23:41:20 -0600

    i've made progress on the linking aspect of the hebi... i think.

--- riesenschlange.py
+++ riesenschlange.py
@@ -48,7 +48,7 @@ class Hebi:
 		self.segment.set_x(self.old_x)
 		self.segment.set_y(self.old_y)
 		self.segment.set_direction(self.direction)
-		#self.update()
+		self.update()
 		
 	def get_x(self):
 		return self.x
@@ -101,7 +101,65 @@ class Hebi:
 			self.move_down()
 		if self.segment: 
 			self.segment.set_direction(self.old_direction)
-			self.segment.update()
+			self.segment.set_x(self.old_x)
+			self.segment.set_y(self.old_y)
+			#self.segment.update()
+
+class NewHebi:
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		self.left = 0
+		self.right = 1
+		self.up = 2
+		self.down = 3
+		self.direction = self.right
+		self.nexthebi = None
+	def set_x(self,x):
+		self.x = x
+	def set_y(self,y):
+		self.y = y
+	def get_x(self):
+		return self.x
+	def get_y(self):
+		return self.y
+	def grow(self):
+		self.nexthebi = NewHebi(self.x, self.y)
+		hebi_list.append(self.nexthebi)
+	def move_up(self):
+		if self.nexthebi:
+			self.nexthebi.set_y(self.y)
+			self.nexthebi.set_x(self.x)
+		self.y -= 1
+		self.direction = self.up
+	def move_down(self):
+		if self.nexthebi:
+			self.nexthebi.set_y(self.y)
+			self.nexthebi.set_x(self.x)
+		self.y += 1
+		self.direction = self.down
+	def move_left(self):
+		if self.nexthebi:
+			self.nexthebi.set_y(self.y)
+			self.nexthebi.set_x(self.x)
+		self.x -= 1
+		self.direction = self.left
+	def move_right(self):
+		if self.nexthebi:
+			self.nexthebi.set_y(self.y)
+			self.nexthebi.set_x(self.x)
+		self.x += 1
+		self.direction = self.right
+	def update(self):
+		if self.direction == self.left:
+			self.move_left()
+		elif self.direction == self.right:
+			self.move_right()
+		elif self.direction == self.up:
+			self.move_up()
+		elif self.direction == self.down:
+			self.move_down()
+
 class Ringo:
 	def __init__(self):
 		self.x = randint(0,(width-1)/line_spacing-1)
@@ -120,7 +178,7 @@ def draw_grid(screen, width, height, line_spacing, color):
 
 hebi_list = []
 ringo_list = []
-myhebi = Hebi()
+myhebi = NewHebi(0,0)
 
 hebi_list.append(myhebi)
 #create a bunch of hebi, just for fun
@@ -175,7 +233,7 @@ while True:
 	for hebi in hebi_list:
 		hebi.update()
 		#hebi.rand_dir()
-		pygame.draw.rect(screen,hebi.get_color(),(hebi.get_x()*line_spacing+1,hebi.get_y()*line_spacing+1,line_spacing-1,line_spacing-1))
+		pygame.draw.rect(screen,cyan,(hebi.get_x()*line_spacing+1,hebi.get_y()*line_spacing+1,line_spacing-1,line_spacing-1))
 	if len(ringo_list) >= 5: show_logo = False 
 	if show_logo: screen.blit(logo,(350,250))
 	pygame.display.flip()
